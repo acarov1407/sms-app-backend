1bcf99a89ef3fb2fbf9c728ab911003f20222864
andres caro velandia <carovelandia07@gmail.com>
2024-09-20 11:26:49 -0500

    loggin firebase

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -8,14 +8,20 @@ export class FirebaseService {
     private app: admin.app.App;
 
     constructor(private configService: ConfigService) {
-        this.app = admin.initializeApp({
-            credential: admin.credential.cert({
-                projectId: this.configService.get<string>('FIREBASE_PROJECT_ID'),
-                privateKey: this.configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
-                clientEmail: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-            }),
-            storageBucket: this.configService.get<string>('FIREBASE_STORAGE_BUCKET')
-        });
+
+        try{
+            this.app = admin.initializeApp({
+                credential: admin.credential.cert({
+                    projectId: this.configService.get<string>('FIREBASE_PROJECT_ID'),
+                    privateKey: this.configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
+                    clientEmail: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+                }),
+                storageBucket: this.configService.get<string>('FIREBASE_STORAGE_BUCKET')
+            });
+        } catch(error) {
+            console.log("ERROR AL INSTANCIA FIREBASE")
+        }
+        
 
     }
 
